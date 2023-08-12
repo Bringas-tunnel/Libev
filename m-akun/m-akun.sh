2e721150195ec1f4dff33bcc0ac453e6af32c637
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-08-12 23:36:32 +0700

    Update m-akun.sh

--- m-akun/m-akun.sh
+++ m-akun/m-akun.sh
@@ -1,95 +1,19 @@
 #!/bin/bash
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/Bringas-tunnel/ipku/main/ipvps > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Bringas-tunnel/ipku/main/ipvps | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Bringas-tunnel/ipku/main/ipvps | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ "$res" = "Expired" ]; then
-Exp="\e[36mExpired\033[0m"
-else
-Exp=$(curl -sS https://raw.githubusercontent.com/Bringas-tunnel/ipku/main/ipvps | grep $MYIP | awk '{print $3}')
-fi
-MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-#########################
 
-# Color Validation
-DF='\e[39m'
-Bold='\e[1m'
-Blink='\e[5m'
-yell='\e[33m'
-red='\e[31m'
-green='\e[32m'
-blue='\e[34m'
-PURPLE='\e[35m'
-cyan='\e[36m'
-Lred='\e[91m'
-Lgreen='\e[92m'
-Lyellow='\e[93m'
-NC='\e[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-LIGHT='\033[0;37m'
-# VPS Information
-#Domain
-domain=$(cat /etc/xray/domain)
 clear 
-echo -e " ========-ACCOUNT MENU-========="
-echo ""
-echo -e " [\e[36m1\e[0m] SSH MENU "
-echo -e " [\e[36m2\e[0m] VMESS MENU"
-echo -e " [\e[36m3\e[0m] VLESS MENU"
-echo -e " [\e[36m4\e[0m] TROJAN MENU"
-echo -e " [\e[33m5\e[0m] BACK TO MENU"
-echo ""               
-echo -e   ""
-read -p " Select menu :  "  opt
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] MENU SSH"
+echo -e " [\e[36m•2\e[0m] MENU VMESS"
+echo -e " [\e[36m•3\e[0m] MENU VLESS"
+echo -e " [\e[36m•4\e[0m] MENU TROJAN"
+echo -e " [\e[36m•5\e[0m] BALIK KE MENU"
+echo -e ""
+read -p " --- >>   " opt
 echo -e ""
 case $opt in
 1) clear ; menu-ssh ; exit ;;
@@ -98,5 +22,5 @@ case $opt in
 4) clear ; menu-trojan ; exit ;;
 5) clear ; menu ; exit ;;
 x) exit ;;
-*) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
+*) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; menu-set ;;
 esac
