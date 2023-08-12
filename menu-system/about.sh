7712c052df29ca9adb084f9ff541cd66e2b0f023
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-08-12 23:53:26 +0700

    Update about.sh

--- menu-system/about.sh
+++ menu-system/about.sh
@@ -3,16 +3,25 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 
 clear
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[0;100;33m        • AutoScript by ADAM SIJA •            \e[0m"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[92    █████╗ ██████╗  █████╗ ███╗   ███╗    ███████╗██╗     ██╗ █████╗" 
-echo -e "\e[92   ██╔══██╗██╔══██╗██╔══██╗████╗ ████║    ██╔════╝██║     ██║██╔══██╗"
-echo -e "\e[92   ███████║██║  ██║███████║██╔████╔██║    ███████╗██║     ██║███████║"
-echo -e "\e[92   ██╔══██║██║  ██║██╔══██║██║╚██╔╝██║    ╚════██║██║██   ██║██╔══██║"
-echo -e "\e[92   ██║  ██║██████╔╝██║  ██║██║ ╚═╝ ██║    ███████║██║╚█████╔╝██║  ██║"
-echo -e "\e[92   ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝    ╚══════╝╚═╝ ╚════╝ ╚═╝  ╚═╝"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " MINAT SCRIPT FULL FITUR ? "
+echo " saya Menyewakan & juga menjual"
+echo " open Source 100% hak milik "
+echo "
+Script full fitur : 
+✅ BOT TELEGRAM
+✅ BACKUP DATA
+✅ DOMAIN FREE (auto pointing)
+✅ LOCK & UNLOCK SSH
+✅ LIMITED DEVIC IP
+⭐HUBUNGI : 085955333616"
+echo " HARGA SEWA SCRIPT FULL FITUR :
+❤️ 1 BULAN : 20 RIBU ( 1 ip )
+❤️ LIFETIME : 25 RIBU ( 1 ip )
+❤️ LIFETIME : 35 RIBU ( 2 ip )
+❤️ LIFETIME : 50 RIBU ( 4 ip )"
+echo " HARGA SCRIPT OPEN SOURCE : 
+🇮🇩 HARGA : 200 RIBU ( Full fitur )
+🇮🇩 HARGA : 150 RIBU ( Semi fitur )"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
